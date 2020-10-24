9bbd2b2c444caa338dfac330b62cdf6df3bc1d32
David <daruiz55@misena.edu.co>
2020-10-24 12:32:36 -0500

    add menus

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+    $('.menu_bar').click(function(){
+        if(contador ==1){
+            $('nav').animate({
+                left: '0'
+            });
+            contador = 0;
+        }
+        else{
+            contador = 1
+            $('nav').animate({
+                left:'-100%'
+            });
+        }
+    });
+    $('.submenu').click(function(){
+        $('.submenu').children(".children").slideToggle();
+    })
+}
